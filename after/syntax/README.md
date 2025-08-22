fab6fb3d7f1be1808cd32b6cd2ff6cc1e4a6908c
fei6409 <fei6409@gmail.com>
2025-08-23 00:22:30 +0800

    feat(syntax)!: support custom keyword highlighting

---
+++ after/syntax/README.md
@@ -0,0 +1,2 @@
+This directory is reserved for the dynamically generated `log.vim` file, which
+contains user-customized syntax definitions.
--- lua/log-highlight.lua
+++ lua/log-highlight.lua
@@ -6,6 +6,21 @@ local defaults = {
     extension = ft,
     filename = {},
     pattern = {},
+    keyword = {
+        error = {},
+        warning = {},
+        info = {},
+        debug = {},
+        pass = {},
+    },
+}
+
+local hl_group_map = {
+    error = 'LogLvError',
+    warning = 'LogLvWarning',
+    info = 'LogLvInfo',
+    debug = 'LogLvDebug',
+    pass = 'LogLvPass',
 }
 
 local function validate_type(name, value, validator)
@@ -42,7 +57,40 @@ local function gen_ft_table(values, category)
     return ft_table
 end
 
----Setup the log-highlight plugin
+---Generate after/syntax/log.vim to highlight custom keywords
+---@param keyword_table table<string, string|string[]>: a table of custom keywords
+local function gen_syntax_file(keyword_table)
+    local plugin_dir = vim.fn.fnamemodify(debug.getinfo(1).source:sub(2), ':h:h')
+    local file_path = plugin_dir .. '/after/syntax/log.vim'
+    local file = io.open(file_path, 'w')
+    local content = {}
+
+    if not file then
+        vim.notify('Failed to write to after/syntax/log.vim', vim.log.levels.ERROR)
+        return
+    end
+
+    validate_type('keyword', keyword_table, 'table')
+    for log_level, words in pairs(keyword_table) do
+        local str = nil
+
+        validate_type('keyword', words, { 'string', 'table' })
+        if type(words) == 'string' then
+            str = words
+        elseif not vim.tbl_isempty(words) then
+            str = table.concat(words, ' ')
+        end
+
+        if hl_group_map[log_level] and str then
+            table.insert(content, 'syn keyword ' .. hl_group_map[log_level] .. ' ' .. str .. '\n')
+        end
+    end
+
+    file:write(table.concat(content))
+    file:close()
+end
+
+---Setup the plugin
 ---@param opts table: a table of options to override the defaults
 function M.setup(opts)
     M.config = vim.tbl_deep_extend('force', defaults, opts)
@@ -52,6 +100,8 @@ function M.setup(opts)
         filename = gen_ft_table(M.config.filename, 'filename'),
         pattern = gen_ft_table(M.config.pattern, 'pattern'),
     }
+
+    gen_syntax_file(M.config.keyword)
 end
 
 return M
